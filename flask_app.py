46f32c31633aa46ea047beead9fc144c21dea3a7
stesre <steve.watson3312@gmail.com>
2024-10-05 17:11:05 +0100

    Added local files to git

--- flask_app.py
+++ flask_app.py
@@ -1,9 +1,20 @@
-from flask import Flask, render_template, abort
+from flask import Flask, render_template, abort, request
 from jobs import jobs
 from summaries import initial_summary
 from blog_posts import blog_posts
+import git
 
 app = Flask(__name__)
+
+@app.route('/update_server', methods=['POST'])
+    def webhook():
+        if request.method == 'POST':
+            repo = git.Repo('path/to/git_repo')
+            origin = repo.remotes.origin
+            origin.pull()
+            return 'Updated PythonAnywhere successfully', 200
+        else:
+            return 'Wrong event type', 400
 
 @app.route('/')
 def index():
